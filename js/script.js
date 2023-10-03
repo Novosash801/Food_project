6e6fef97621f9f70dfebc3447f820889ec10a671
Alex <chernousov013@gmail.com>
2023-10-03 15:29:31 +0700

    add axios lib

--- js/script.js
+++ js/script.js
@@ -193,6 +193,20 @@ document.addEventListener('DOMContentLoaded', function() { // Создаем о�
             this.parent.append(element);
         }
     }
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-   
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+        return await res.json();
+    };
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
 }); 
